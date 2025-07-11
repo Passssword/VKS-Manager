9a55e3a2d66531833988961c6150c64880d376d8
Ilya <Stepanenko.my-mail@yandex.ru>
2025-07-11 16:18:08 +1100

    изменены стили меню навигации

--- web/scripts/sidebar.js
+++ web/scripts/sidebar.js
@@ -14,14 +14,15 @@ document.addEventListener('DOMContentLoaded', async function() {
         sidebarNickname.innerHTML = authData.Nickname
         sidebarMenu.innerHTML = `
             <p class="munu_caption">ВКС События:</p>
-            <a href="tableListVKS.html" class="menuBtn">Таблица ВКС</a> <br />
-            <a href="reportsPage.html" class="menuBtn">Отчеты ВКС</a> <br />
+            <a href="tableListVKS.html" class="menu_button">Таблица ВКС</a> <br />
+            <a href="reportsPage.html" class="menu_button">Отчеты ВКС</a> <br />
         `
         if (authData.rolle == "Admin") {
             sidebarAdminka.innerHTML = `
             <p class="munu_caption">Настройки:</p>
-            <a href="settings.html" class="menuBtn">Settins</a> <br />
-            <a href="usersPage.html" class="menuBtn">Users</a> <br />
+            <a href="settings.html" class="menu_button">Settins</a> <br />
+            <a href="usersPage.html" class="menu_button">Users</a> <br />
+            <a href="#" class="menu_button">Судьи</a> <br />
         `
     }
         
